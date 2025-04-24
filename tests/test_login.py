f52dad670c9320220c3112a635b093508d6873bc
YashPayer2510 <yash.payer@mochatechnologies.com>
2025-04-24 15:22:49 +0530

    changes to login

--- tests/test_login.py
+++ tests/test_login.py
@@ -23,6 +23,7 @@ valid_username = os.getenv('VALIDLOGINUSERNAME').split(',')
 valid_password = os.getenv('VALIDLOGINPASSWORD').split(',')
 invalid_username =os.getenv('INVALIDUSERNAME').split(',')
 invalid_password =os.getenv('INVALIDLOGINPASSWORD').split(',')
+invalid_format_username =os.getenv('INVALIDFORMATMAIL').split(',')
 
 #expectedmessage = login_test_data['expectederrormsg']
 
@@ -43,38 +44,60 @@ def test_valid_login(setup, login_test_data, username, password):
     logger.info("The test case passed")
 
 #test with invalid credentials and validation should be displayed
-@pytest.mark.parametrize("username, password", zip(invalid_username, invalid_password))
-def test_invalid_login(setup, login_test_data,username, password ):
+@pytest.mark.parametrize("username", zip(invalid_username))
+def test_invalid_email_login(setup, login_test_data,username):
     driver= setup
+    login_page = LoginPage(driver)
+    login_page.enter_username(username)
+    #login_page.enter_password(password)
+    login_page.click_loginbutton()
+    login_page.incorrect_email_validation(login_test_data)
+
+@pytest.mark.parametrize("username, password", zip(valid_username, invalid_password))
+def test_invalid_password_login(setup, login_test_data,username, password ):
+    driver = setup
     login_page = LoginPage(driver)
     login_page.enter_username(username)
     login_page.enter_password(password)
     login_page.click_loginbutton()
     login_page.failed_login(login_test_data)
 
+
 #test with blank username and click on login button
-def test_username_validation(setup, login_test_data):
+def test_blank_username_validation(setup, login_test_data):
     driver = setup
     login_page = LoginPage(driver)
-    login_page.enter_password(valid_password[0])
+    #login_page.enter_password(valid_password[0])
+    login_page.click_username()
     login_page.click_loginbutton()
     login_page.username_blank_validation(login_test_data)
 
 #test with blank password and click on login button
-def test_password_validation(setup,login_test_data):
+def test_blank_password_validation(setup,login_test_data):
     driver = setup
     login_page = LoginPage(driver)
     login_page.enter_username(valid_username[0])
+    login_page.click_password()
     login_page.click_loginbutton()
     login_page.password_blank_validation(login_test_data)
 
 #test with blank user and password and click on login button
-def test_usernasmepassword_validation(setup,login_test_data):
+def test_blank_usernamepassword_validation(setup,login_test_data):
     driver = setup
     login_page = LoginPage(driver)
     login_page.click_loginbutton()
     login_page.username_blank_validation(login_test_data)
     login_page.password_blank_validation(login_test_data)
+
+@pytest.mark.parametrize("username", zip(invalid_format_username))
+def test_invalid_email_validation(setup, login_test_data, username):
+    driver = setup
+    login_page = LoginPage(driver)
+    login_page.enter_username(username)
+    login_page.click_loginbutton()
+    login_page.invalid_email_validation(login_test_data)
+
+
 
 
 
